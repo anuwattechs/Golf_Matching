2634d1a4e905d481089a232cd16795191b23401c
sataporn1995 <satapornchai2538@gmail.com>
2024-09-26 18:08:31 +0700

    Updated Files

--- server/src/services/users/dto/forgot-password.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsEmail } from 'class-validator';
-
-export class ForgotPasswordDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-}
--- server/src/services/users/dto/index.ts
+++ server/src/services/users/dto/index.ts
@@ -1,8 +1,6 @@
 export * from './register.dto';
 export * from './change-password.dto';
-// export * from './forgot-password.dto';
 export * from './reset-password.dto';
-// export * from './verify-forgot-password.dto';
 export * from './login.dto';
 export * from './update-personal-info.dto';
 export * from './change-invite-mode.dto';
