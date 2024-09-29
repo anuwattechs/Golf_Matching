d0575b2e3051a63a4481f57ff07c6766f6d6eb6a
sataporn1995 <satapornchai2538@gmail.com>
2024-09-29 17:11:27 +0700

    Initial commit

--- server/src/schemas/users.schema.ts
+++ server/src/schemas/users.schema.ts
@@ -1,10 +1,10 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import { AuthProvidersEnum } from 'src/shared/enums';
-import { Exclude } from 'class-transformer';
-import { v4 as uuidv4 } from 'uuid';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { AuthProvidersEnum } from "src/shared/enums";
+import { Exclude } from "class-transformer";
+import { v4 as uuidv4 } from "uuid";
 
-@Schema({ timestamps: true })
+@Schema({ collection: "Users", timestamps: true })
 export class User extends Document {
   @Prop({
     type: String, // Define the type of _id as String for UUID
@@ -12,7 +12,19 @@ export class User extends Document {
   })
   _id: string;
 
-  @Prop({ unique: true, nullable: true, type: String })
+  @Prop({ required: true, type: String })
+  firstName: string;
+
+  @Prop({ required: true, type: String })
+  lastName: string;
+
+  @Prop({ default: null })
+  nickName: string;
+
+  @Prop({ default: null })
+  dateOfBirth: string;
+
+  @Prop({ unique: true, required: true, type: String }) //! Email, Phone, Social
   email: string | null;
 
   @Prop({ nullable: true, select: false })
@@ -32,55 +44,72 @@ export class User extends Document {
   @Prop({ nullable: true, index: true, type: String })
   socialId: string | null;
 
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  country: string;
+
+  @Prop({ required: true })
+  location: string;
+
+  @Prop({ default: "" })
+  occupation: string;
+
+  @Prop({ default: [], type: [String] })
+  lifestyle: string[];
+
+  @Prop({ default: null }) //! Year 4 digits
+  startedGolf: number;
+
+  @Prop({ default: null })
+  avgScore: number;
+
+  @Prop({ default: null })
+  favoriteCourse: string;
+
+  @Prop({ default: null })
+  holesInOne: number;
+
+  @Prop({ default: null })
+  bestScore: number;
+
+  @Prop({ default: "" })
+  clubs: string;
+
+  @Prop({ default: "" })
+  introduction: string;
+
+  @Prop({ default: true })
+  isInvited: boolean;
+
   @Prop({ default: false })
-  isVerified: boolean;
+  isActived: boolean;
 
-  @Prop({ type: String })
-  otp: string; // สำหรับ OTP
+  @Prop({ default: null })
+  activedAt: Date;
 
-  @Prop({ type: Date })
-  otpExpired: Date;
+  // @Prop({ nullable: true, type: String })
+  // phone: string | null;
 
-  @Prop({ default: false })
-  otpSent: boolean;
+  // @Prop({ default: Date.now })
+  // lastLogin: Date;
 
-  @Prop({
-    nullable: true,
-    type: String,
-    unique: true,
-  })
-  phone: string | null;
+  // @Prop({default: Date.now})
+  // createdAt: Date;
 
-  @Prop({ nullable: true, type: String })
-  firstName: string | null;
+  // @Prop({default: Date.now})
+  // updatedAt: Date;
 
-  @Prop({ nullable: true, type: String })
-  lastName: string | null;
-
-  @Prop({
-    default: Date.now,
-  })
-  lastLogin: Date;
-
-  @Prop({
-    default: Date.now,
-  })
-  createdAt: Date;
-
-  @Prop({
-    default: Date.now,
-  })
-  updatedAt: Date;
-
-  @Prop({ nullable: true })
-  deletedAt: Date | null;
+  // @Prop({ nullable: true })
+  // deletedAt: Date | null;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
-UserSchema.pre('save', function (next) {
+UserSchema.pre("save", function (next) {
   const user = this as User;
-  if (!user.isModified('password')) return next();
+  if (!user.isModified("password")) return next();
   user.previousPassword = user.password;
   next();
 });
