a768db95c667773a296a2e5a7ac9eee2a815d013
anuwatthisuka <Anuwat57766@gmail.com>
2024-10-01 20:09:52 +0700

    Refactor enum and schema imports

--- server/dist/src/app/modules/auth/auth.controller.d.ts
+++ server/dist/src/app/modules/auth/auth.controller.d.ts
@@ -1,7 +1,5 @@
 import { AuthService } from './auth.service';
-import { CreateUserDto } from '../users/dto/create-user.dto';
 export declare class AuthController {
     private readonly authService;
     constructor(authService: AuthService);
-    register(createUserDto: CreateUserDto): Promise<void>;
 }
--- server/dist/src/shared/enums/index.d.ts
+++ server/dist/src/shared/enums/index.d.ts
@@ -1,1 +1,2 @@
 export * from './auth-providers.enum';
+export * from './gender.enum';
