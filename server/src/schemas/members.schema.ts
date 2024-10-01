8d0e4361c4dc5dc7d8d7348e9b09a8619a8d8695
anuwatthisuka <Anuwat57766@gmail.com>
2024-10-01 20:45:40 +0700

    Refactor 'isActivated' property in Member schema

--- server/src/schemas/members.schema.ts
+++ server/src/schemas/members.schema.ts
@@ -84,11 +84,7 @@ export class Member extends Document {
   isInvited: boolean;
 
   @Prop({ default: false })
-<<<<<<< HEAD
-  isActivated: boolean;
-=======
   isActived: boolean;
->>>>>>> a768db95c667773a296a2e5a7ac9eee2a815d013
 
   @Prop({ default: null })
   activedAt: Date;
