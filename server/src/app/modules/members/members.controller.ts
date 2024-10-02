323c68240bde20e474ce54401a1b2fdb262edc57
sataporn1995 <satapornchai2538@gmail.com>
2024-10-02 15:58:45 +0700

    Updated

--- server/src/app/modules/members/members.controller.ts
+++ server/src/app/modules/members/members.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Put,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/app/modules/auth/guard/jwt-auth.guard';
 import { MembersService } from './members.service';
 import { Request } from 'express';
 import { JwtPayloadType } from 'src/app/modules/auth/strategy/jwt-payload.type';
 import { ResponseMessage } from 'src/app/common/decorator/response-message.decorator';
+import { UpdateProfileDto, ChangeInviteModeDto } from './dto';
 
 @Controller('members')
 export class MembersController {
@@ -11,10 +20,30 @@ export class MembersController {
 
   @Get('profile')
   @UseGuards(JwtAuthGuard)
-  @ResponseMessage('User reset password successfully')
+  @ResponseMessage('Personal info retrieved successfully')
   async findOneProsonalInfo(
     @Req() req: Request & { decoded: JwtPayloadType },
   ) /*: Promise<LoginResponseDto> */ {
     return await this.membersService.findOneProsonalInfo(req.decoded);
+  }
+
+  @Put()
+  @UseGuards(JwtAuthGuard)
+  @ResponseMessage('Profile updated successfully')
+  async updateProfile(
+    @Body() body: UpdateProfileDto,
+    @Req() req: Request & { decoded: JwtPayloadType },
+  ) /*: Promise<LoginResponseDto> */ {
+    return await this.membersService.updateProfile(body, req.decoded);
+  }
+
+  @Patch('change-invite-mode')
+  @UseGuards(JwtAuthGuard)
+  @ResponseMessage('Invite mode changed successfully')
+  async changeInviteMode(
+    @Body() body: ChangeInviteModeDto,
+    @Req() req: Request & { decoded: JwtPayloadType },
+  ) /*: Promise<LoginResponseDto> */ {
+    return await this.membersService.changeInviteMode(body, req.decoded);
   }
 }
