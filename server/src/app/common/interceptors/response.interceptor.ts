50a3d8246adc35b6b02aee9a35bb8f444bcc8e24
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-10-16 18:03:41 +0700

    Refactor i18n files for Thai and English languages

--- server/src/app/common/interceptors/response.interceptor.ts
+++ server/src/app/common/interceptors/response.interceptor.ts
@@ -8,78 +8,87 @@ import {
 } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-// import { format } from 'date-fns';
 import { Reflector } from '@nestjs/core';
+import { I18nContext } from 'nestjs-i18n';
 import { RESPONSE_MESSAGE_METADATA } from '../decorator/response-message.decorator';
 
 export type Response<T> = {
   status: boolean;
   statusCode: number;
-  // path: string;
   message: string;
   data: T;
-  // timestamp: string;
 };
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  constructor(private reflector: Reflector) {}
+  constructor(private readonly reflector: Reflector) {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<Response<T>> {
     return next.handle().pipe(
-      map((res: unknown) => this.responseHandler(res as T, context)),
+      map((res: T) => this.createSuccessResponse(res, context)),
       catchError((err: HttpException) =>
-        throwError(() => this.errorHandler(err, context)),
+        throwError(() => this.createErrorResponse(err, context)),
       ),
     );
   }
 
-  errorHandler(exception: HttpException, context: ExecutionContext) {
+  private createErrorResponse(
+    exception: HttpException,
+    context: ExecutionContext,
+  ) {
     const ctx = context.switchToHttp();
     const response = ctx.getResponse();
-    // const request = ctx.getRequest();
+    const status = exception.getStatus
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+    const i18nMessage = I18nContext.current().t(
+      `status-code.${status}`,
+    ) as string;
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
       status: false,
       statusCode: status,
-      // path: request.url,
-      message: exception.message,
-      data:
-        status === HttpStatus.BAD_REQUEST
-          ? typeof (exception.getResponse() as any).message === 'string'
-            ? null
-            : (exception.getResponse() as any).message || null
-          : null,
-      // timestamp: format(new Date().toISOString(), 'yyyy-MM-dd HH:mm:ss'),
+      message: i18nMessage,
+      data: this.extractErrorMessage(exception),
     });
   }
 
-  responseHandler(res: T, context: ExecutionContext) {
+  private extractErrorMessage(exception: HttpException) {
+    const response = exception.getResponse();
+    if (typeof response === 'string') {
+      return null;
+    }
+    return (response as any).message || null;
+  }
+
+  private createSuccessResponse(
+    res: T,
+    context: ExecutionContext,
+  ): Response<T> {
     const ctx = context.switchToHttp();
     const response = ctx.getResponse();
-    // const request = ctx.getRequest();
     const statusCode = response.statusCode;
-    const message =
-      this.reflector.get<string>(
-        RESPONSE_MESSAGE_METADATA,
-        context.getHandler(),
-      ) || 'success';
+    const message = this.getResponseMessage(context);
 
     return {
       status: true,
-      // path: request.url,
       statusCode,
-      message: message,
+      message,
       data: res,
-      // timestamp: format(new Date().toISOString(), 'yyyy-MM-dd HH:mm:ss'),
     };
+  }
+
+  private getResponseMessage(context: ExecutionContext): string {
+    const customMessage = this.reflector.get<string>(
+      RESPONSE_MESSAGE_METADATA,
+      context.getHandler(),
+    );
+    return (
+      customMessage || (I18nContext.current().t('common.SUCCESS') as string)
+    );
   }
 }
