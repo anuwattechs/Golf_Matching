d70070529d04e10db547138d919d53ac8aeeea76
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-10-17 16:48:18 +0700

    update

--- server/src/app/common/interceptors/response.interceptor.ts
+++ server/src/app/common/interceptors/response.interceptor.ts
@@ -11,7 +11,7 @@ import { catchError, map } from 'rxjs/operators';
 import { Reflector } from '@nestjs/core';
 import {
   RESPONSE_MESSAGE_METADATA,
-  ResponseMessage as ResponseMessageType,
+  ResponseMessage
 } from '../decorator/response-message.decorator';
 import { I18nContext } from 'nestjs-i18n';
 import { I18nPath } from 'src/generated/i18n.generated';
